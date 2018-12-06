5598d9aeb3afd6fef9fe7e94f2448f1986988e26
John_Q <qiucheng93@163.com>
2018-12-06 22:36:32 +0800

    initial commit

---
+++ Assets/QFramework/ExportUnitypackage/ExportUnityPackage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace QFramework
+{
+    public class ExportUnityPackage
+    {
+#if UNITY_EDITOR
+        [MenuItem("QFramework/1. ExportPackage")]
+        private static void MenuClicked()
+        {
+            //将导出的文件路径直接复制到剪切板
+            string copyFileName = "QFramework_" + DateTime.Now.ToString("yyyyMMdd_HH");
+            GUIUtility.systemCopyBuffer = copyFileName;
+
+            //直接一键做完整个导出流程
+            string assetPathNmae = "Assets/QFramework";
+            string fileName = "QFramework_" + DateTime.Now.ToString("yyyyMMdd_HH") + ".unitypackage";
+            AssetDatabase.ExportPackage(assetPathNmae, fileName, ExportPackageOptions.Recurse);
+        }
+#endif
+    }
+}
---
+++ Assets/QFramework/ExportUnitypackage/OpenUnityPackageFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace QFramework
+{
+    public class OpenUnityPackageFile
+    {
+#if UNITY_EDITOR
+        [MenuItem("QFramework/2. OpenInFolder %e")]
+        private static void MenuClicked()
+        {
+            EditorApplication.ExecuteMenuItem("QFramework/1. ExportPackage");
+            Application.OpenURL("file:///" + Path.Combine(Application.dataPath , "../"));
+        }
+#endif
+    }
+}
